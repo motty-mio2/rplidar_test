645e91280adb68f17d91cb937cc7385a3f6e423c
motty <motty.mio2@gmail.com>
2025-10-06 21:52:48 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,27 @@
 
 #include <iostream>
 #include <limits>
-#include <vector>
 
 #include "arc_intersection.hpp"
 #include "degree2position.hpp"
 #include "flags.hpp"
-#include "generate_color.hpp"
-
+#include "lidar_obj.hpp"
+#include "visualizer.hpp"
+#include "zenoh_wrapper.hpp"
 using namespace sl;
 
 #ifndef _countof
 #define _countof(_Array) (int)(sizeof(_Array) / sizeof(_Array[0]))
 #endif
 
-constexpr int IMG_SIZE = 600;
-
 volatile sig_atomic_t ctrl_c_pressed = 0;
 
 void ctrlc_handler(int) { ctrl_c_pressed = 1; }
 
 int main(int argc, char *argv[]) {
+  gflags::SetUsageMessage("How To Use");
+  gflags::SetVersionString("1.0.0");
+
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   std::cout << "Hello, RPLIDAR!" << std::endl;
 
@@ -56,26 +57,14 @@ int main(int argc, char *argv[]) {
 
   // fetech result and print it out...
 
-  float angle_step = 360.0f / FLAGS_num;
-  std::vector<float> near;
-
-  for (auto i = 0; i < FLAGS_num; ++i) {
-    near.push_back(std::numeric_limits<float>::infinity());
-  }
-
   while (!ctrl_c_pressed) {
-    int loop_count = 0;
-
-    for (auto &v : near) {
-      v = std::numeric_limits<float>::infinity();
-    }
-
     sl_lidar_response_measurement_node_hq_t nodes[8192];
     size_t count = _countof(nodes);
 
     sl_result op_result = lidar->grabScanDataHq(nodes, count);
 
-    cv::Mat img = cv::Mat::zeros(IMG_SIZE, IMG_SIZE, CV_8UC3);
+    auto data = LiDAR_DATA();
+
     if (SL_IS_OK(op_result)) {
       lidar->ascendScanData(nodes, count);
 
@@ -84,35 +73,17 @@ int main(int argc, char *argv[]) {
             47) {
           continue;
         }
+
         float degree = (nodes[pos].angle_z_q14 * 90.f) / 16384.f;
-        float dist =
-            std::min(FLAGS_max_dist, (double)nodes[pos].dist_mm_q2 / 4.0f) *
-            (IMG_SIZE / 2.0f) / FLAGS_max_dist;
+        float dist = std::min(FLAGS_max_dist,  //
+                              (double)nodes[pos].dist_mm_q2 / 4.0f);
 
-        loop_count += (degree > (loop_count + 1) * angle_step);
-
-        near[loop_count] = std::min(near[loop_count], dist);
-
-        cv::drawMarker(
-            img, degree2position(IMG_SIZE / 2, IMG_SIZE / 2, degree, dist),
-            //  std::min(dist, IMG_SIZE / 2.0f)),
-            cv::Scalar(255, 0, 0), cv::MARKER_CROSS, 10, 2);
+        data.insert(degree, dist);
       }
-      std::cout << near[0] << " " << near[1] << " " << near[2] << " " << near[3]
-                << std::endl;
     }
 
-    for (auto i = 0; i < FLAGS_num; ++i) {
-      near[i] = std::min(near[i], IMG_SIZE / 2.0f);
-
-      cv::ellipse(img, cv::Point2d(IMG_SIZE / 2, IMG_SIZE / 2),
-                  cv::Size(near[i], near[i]), angle_step, angle_step * (i - 1),
-                  angle_step * i, generate_color(i, FLAGS_num), 2);
-    }
-    cv::imshow("Test Window", img);
-    if (cv::waitKey(1) == 'q') {
-      break;
-    }
+    visualize(data.get());
+    data.clear();
   }
 
   lidar->stop();
