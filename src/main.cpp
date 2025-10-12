1332ea67874a5e765757d2bff2ec9c96dfec6cef
motty <motty.mio2@gmail.com>
2025-10-12 12:21:44 +0900

    rplidar wrapperに置き換え（未テスト）

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <gflags/gflags.h>
 #include <rplidar.h>
 #include <signal.h>
 
@@ -8,13 +9,12 @@
 #include "degree2position.hpp"
 #include "flags.hpp"
 #include "lidar_data.hpp"
+#include "lidar_device/rplidar_wrapper.hpp"
 #include "visualizer.hpp"
 #include "zenoh_wrapper.hpp"
-using namespace sl;
 
-#ifndef _countof
-#define _countof(_Array) (int)(sizeof(_Array) / sizeof(_Array[0]))
-#endif
+DECLARE_string(d);
+DECLARE_double(max_dist);
 
 volatile sig_atomic_t ctrl_c_pressed = 0;
 
@@ -25,69 +25,20 @@ int main(int argc, char *argv[]) {
   gflags::SetVersionString("1.0.0");
 
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-  std::cout << "Hello, RPLIDAR!" << std::endl;
 
   signal(SIGINT, ctrlc_handler);
 
-  ///  Create a communication channel instance
-  IChannel *_channel;
-  Result<IChannel *> channel = createSerialPortChannel(FLAGS_d, 115200);
+  RplidarWrapper lidar(FLAGS_d, FLAGS_max_dist);
 
-  ///  Create a LIDAR driver instance
-  ILidarDriver *lidar = *createLidarDriver();
-  auto res = (*lidar).connect(*channel);
-  if (SL_IS_OK(res)) {
-    sl_lidar_response_device_info_t deviceInfo;
-    res = (*lidar).getDeviceInfo(deviceInfo);
-    if (SL_IS_OK(res)) {
-      printf("Model: %d, Firmware Version: %d.%d, Hardware Version: %d\n",
-             deviceInfo.model, deviceInfo.firmware_version >> 8,
-             deviceInfo.firmware_version & 0xffu, deviceInfo.hardware_version);
-    } else {
-      fprintf(stderr, "Failed to get device information from LIDAR %08x\r\n",
-              res);
-    }
-  } else {
-    fprintf(stderr, "Failed to connect to LIDAR %08x\r\n", res);
-  }
-
-  lidar->setMotorSpeed();
-  // start scan...
-  lidar->startScan(0, 1);
-
-  // fetech result and print it out...
+  auto data = LiDARDataWrapper();
 
   while (!ctrl_c_pressed) {
-    sl_lidar_response_measurement_node_hq_t nodes[8192];
-    size_t count = _countof(nodes);
+    data.clear();
 
-    sl_result op_result = lidar->grabScanDataHq(nodes, count);
-
-    auto data = LiDARDataWrapper();
-
-    if (SL_IS_OK(op_result)) {
-      lidar->ascendScanData(nodes, count);
-
-      for (int pos = 0; pos < (int)count; ++pos) {
-        if (nodes[pos].quality >> SL_LIDAR_RESP_MEASUREMENT_QUALITY_SHIFT !=
-            47) {
-          continue;
-        }
-
-        float degree = (nodes[pos].angle_z_q14 * 90.f) / 16384.f;
-        float dist = std::min(FLAGS_max_dist,  //
-                              (double)nodes[pos].dist_mm_q2 / 4.0f);
-
-        data.insert(degree, dist);
-      }
+    if (lidar.get(data)) {
+      visualize(data.get());
     }
 
-    visualize(data.get());
-    data.clear();
+    return 0;
   }
-
-  lidar->stop();
-  lidar->setMotorSpeed(0);
-
-  return 0;
 }
