e2e9866d0f70392cdac57f6fe344e527cfccfa3d
motty <motty.mio2@gmail.com>
2025-10-16 00:09:50 +0900

    データ型の修正

--- src/lidar_types/lidar_data.cpp
+++ src/lidar_types/lidar_data.cpp
@@ -1,36 +1,11 @@
 #include "lidar_types/lidar_data.hpp"
 
-void to_json(nlohmann::json j, const LidarMetadata &t) {
-  j = nlohmann::json{{"x", t.x},
-                     {"y", t.y},
-                     {"min_rad", t.min_rad},
-                     {"max_rad", t.max_rad},
-                     {"max_dist", t.max_dist}};
-}
-
-void from_json(const nlohmann::json &j, LidarMetadata &t) {
-  j.at("x").get_to(t.x);
-  j.at("y").get_to(t.y);
-  j.at("min_rad").get_to(t.min_rad);
-  j.at("max_rad").get_to(t.max_rad);
-  j.at("max_dist").get_to(t.max_dist);
-}
-
-std::vector<uint8_t> LidarMetadata::dump() {
-  return nlohmann::json::to_msgpack(*this);
-}
-
-LidarMetadata::LidarMetadata(const std::vector<uint8_t> bin_data) {
-  auto j = nlohmann::json::from_msgpack(bin_data);
-  j.get_to(*this);
-}
-
 void to_json(nlohmann::json &j, const LiDARDataWrapper &p) {
   j = nlohmann::json{{"data", p.data}};
 }
 
 void from_json(const nlohmann::json &j, LiDARDataWrapper &p) {
-  j.at("data").get_to(p.data);
+  j.get_to(p.data);
 }
 
 LiDARDataWrapper::LiDARDataWrapper(const std::vector<uint8_t> bin_data) {
