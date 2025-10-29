18e3e690e7196f7567c7f4cc57ea100f8ff73829
motty <motty.mio2@gmail.com>
2025-10-29 23:37:02 +0900

    最大距離なら描画しない

--- include/visualizer.hpp
+++ include/visualizer.hpp
@@ -12,10 +12,12 @@ cv::Mat visualize(LiDARData data, std::string window_name, uint32_t num = 4,
                   uint32_t image_size = 600, float max_distance = 1000.0) {
   float angle_step = 360.0f / num;
 
+  float max_visual_distance = image_size / 2.0f;
+
   std::vector<float> near;
 
   for (auto i = 0; i < num; ++i) {
-    near.push_back(image_size / 2.0f);
+    near.push_back(max_visual_distance);
   }
 
   cv::Mat img = cv::Mat::zeros(image_size, image_size, CV_8UC3);
@@ -28,10 +30,13 @@ cv::Mat visualize(LiDARData data, std::string window_name, uint32_t num = 4,
     int index = static_cast<int>(degree / angle_step);
 
     near[index] =
-        std::min(near[index], distance * (image_size / 2.0f) / max_distance);
+        std::min(near[index], distance * (max_visual_distance) / max_distance);
   }
 
   for (auto i = 0; i < num; ++i) {
+    if (near[i] >= (max_visual_distance)) {
+      continue;
+    }
     cv::ellipse(img, cv::Point2d(image_size / 2, image_size / 2),
                 cv::Size(near[i], near[i]), angle_step, angle_step * (i - 1),
                 angle_step * i, generate_color(i, num), 2);
