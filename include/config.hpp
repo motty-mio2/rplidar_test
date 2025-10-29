079a6fee994e580ca4e55cc4747175b50ae7d878
motty <motty.mio2@gmail.com>
2025-10-29 22:42:13 +0900

    送信側作成

---
+++ include/config.hpp
@@ -0,0 +1,25 @@
+#include <toml.hpp>
+
+#include "gflags/gflags.h"
+
+DECLARE_string(c);
+
+toml::value get_config(std::string name) {
+  std::filesystem::path environment_file = std::filesystem::path(FLAGS_c);
+
+  if (environment_file.empty()) {
+    if (const char* home = std::getenv("XDG_CONFIG_HOME")) {
+      environment_file = std::filesystem::path(home) / "roboapp/config.toml";
+    } else if (const char* home = std::getenv("HOME")) {
+      environment_file =
+          std::filesystem::path(home) / ".config/roboapp/config.toml";
+    } else {
+      throw std::runtime_error("Cannot determine config file path");
+    }
+  }
+
+  if (!std::filesystem::exists(environment_file)) {
+    throw std::runtime_error("Config file not found");
+  }
+  return toml::parse(environment_file).at("lidar").at("devices").at(name);
+}
--- src/sender.cpp
+++ src/sender.cpp
@@ -8,20 +8,21 @@
 #include <string>
 #include <thread>
 
+#include "config.hpp"
 #include "lidar_device/random_lidar.hpp"
 #include "lidar_device/rplidar_wrapper.hpp"
 #include "lidar_types/lidar_data.hpp"
 #include "zenoh.hxx"
 
-DEFINE_string(d, "/dev/ttyUSB0", "path/to/device");
-DEFINE_double(m, 1000, "maximum distance in mm");
-DEFINE_string(b, "random", "lidar backend (rplidar or random)");
+DEFINE_string(
+    c, "", "config file path: Default `$XDG_CONFIG_DIR/roboapp/config.toml`");
+DEFINE_string(n, "", "LiDAR name in config file");
 
 volatile sig_atomic_t ctrl_c_pressed = 0;
 
 void ctrlc_handler(int) { ctrl_c_pressed = 1; }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   // Flag Setup
   gflags::SetUsageMessage("How To Use");
   gflags::SetVersionString("1.0.0");
@@ -29,26 +30,33 @@ int main(int argc, char *argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   signal(SIGINT, ctrlc_handler);
 
-  // Zenoh Setup
+  std::map<std::string, std::string> config_map;
 
+  auto lidar_config = get_config(FLAGS_n);
+
+  std::unique_ptr<MockLiDAR> lidar;
+
+  if (lidar_config.at("backend").as_string() == "rplidar") {
+    std::cout << "RPLIDAR selected" << std::endl;
+    lidar = std::make_unique<RplidarWrapper>(
+        lidar_config.at("device").as_string(),
+        toml::find_or(lidar_config, "max_distance", 1000));
+  } else if (lidar_config.at("backend").as_string() == "random") {
+    std::cout << "RandomLiDAR selected" << std::endl;
+    lidar = std::make_unique<RandomLiDAR>(
+        toml::find_or(lidar_config, "max_distance", 1000));
+  } else {
+    std::cerr << "Unknown backend: " << lidar_config.at("backend").as_string()
+              << std::endl;
+    return 1;
+  }
+
+  // Zenoh Setup
   auto config = zenoh::Config::create_default();
   config.insert_json5(Z_CONFIG_ADD_TIMESTAMP_KEY, "true");
 
   auto session = zenoh::Session(std::move(config));
   auto publisher = session.declare_publisher(zenoh::KeyExpr("lidar/data"));
-  auto metadata_publisher =
-      session.declare_publisher(zenoh::KeyExpr("lidar/metadata"));
-
-  std::unique_ptr<MockLiDAR> lidar;
-
-  if (FLAGS_b == "rplidar") {
-    lidar = std::make_unique<RplidarWrapper>(FLAGS_d, FLAGS_m);
-  } else if (FLAGS_b == "random") {
-    lidar = std::make_unique<RandomLiDAR>(FLAGS_m);
-  } else {
-    std::cerr << "Invalid backend: " << FLAGS_b << std::endl;
-    return 1;
-  }
 
   auto data = LiDARDataWrapper();
 
